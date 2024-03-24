b8c6b9e6a8c9ce2ae7d14f715317ab577e907249
hyugoshirai <henrique.shirai@ufl.edu>
2024-03-24 13:49:28 -0400

    Merge branch 'main' of  https://github.com/hyugoshirai/pontal-app

--- app.R
+++ app.R
@@ -9,15 +9,34 @@ library(rgdal)
 
 # Define UI for application that draws a map
 ui <- fluidPage(
-  titlePanel("Draw Polygons and Add Labels"),
-  mainPanel(
-    leafletOutput("map"),
-    uiOutput("label_input")  # Add this line to include the dynamic UI element
+  titlePanel("Mapping the Pontal"),
+  sidebarLayout(
+      sidebarPanel(
+        # Add text to the sidebar
+        HTML("<h3> Draw Polygons and Add Labels </h3>"),
+        p("This tool is designed to draw polygon into the map and adding labels to them."),
+        tags$hr(),  # Add a horizontal line
+        p("With the 'Draw a polygon' tool, add polygons to the map."),
+        tags$hr(),  # Add a horizontal line
+        p("Finish the polygon and enter the label in the text box."),
+        tags$hr(),  # Add a horizontal line
+        p("After finishing the text, click Ádd Label' button to add it to the centroid of the polygon")
+      ),
+    mainPanel(
+      leafletOutput("map"),
+      uiOutput("label_input")  # Add this line to include the dynamic UI element
+    )
   )
 )
 
-
+# Define server logic
 server <- function(input, output, session) {
+  
+  # Render instructions text
+  output$instructions <- renderText({
+    "Insert your instructions here..."
+  })
+  
   # Initialize reactive values to store drawn polygons
   drawn_polygons <- reactiveValues(polygons = list())
   label_text <- reactiveVal(NULL)
